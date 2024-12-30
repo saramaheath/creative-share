c8946bbd1406d79ad13aaebe1847697cd1881145
Sara Heath <saramaheath@gmail.com>
2024-12-30 14:05:42 -0600

    start home and profile page file structure

---
+++ src/components/nav.tsx
@@ -0,0 +1,10 @@
+import NavDropDown from '@/components/navDropDown';
+
+export default function Nav(){
+    return (
+    <>
+        <h3>Creative Share</h3>
+        <NavDropDown></NavDropDown>
+    </>
+    );
+}
---
+++ src/components/navDropDown.tsx
@@ -0,0 +1,7 @@
+export default function NavDropDown(){
+    return (
+    <>
+        <button>drop down</button>
+    </>
+    );
+}
