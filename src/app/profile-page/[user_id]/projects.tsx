183e84da31053b554e72d476b470e6c3a7e72076
Sara Heath <saramaheath@gmail.com>
2025-01-03 18:01:54 -0600

    tab styles

--- src/app/profile-page/[user_id]/projects.tsx
+++ src/app/profile-page/[user_id]/projects.tsx
@@ -6,8 +6,8 @@ export default function Projects(){
     const projects = ['/sample_proj_img.jpg', '/sample_proj_img.jpg', '/sample_proj_img.jpg', '/sample_proj_img.jpg'];
 
     return (
-        <div className="p-2 flex flex-col section-pwhite rounded-md w-7/8 m-8 ">
-            <div className="grid grid-cols-4">
+        <div className="p-2 mx-2 flex flex-col section-pwhite border-x-2 border-b-2 rounded-b-md border-pdark">
+            <div className="grid grid-cols-4 m-8">
                 {projects.map((img, index) =>
                     <img key={index} src={img} alt="project-image" className="size-24 rounded-md m-2"/>
                 )}
