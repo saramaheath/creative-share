2723928630b6457e5a905859db1dd92cfbb91d73
sarheathRA <sara.heath@realtyads.com>
2025-01-14 13:56:34 -0600

    layout comments and posts

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -6,12 +6,16 @@ export default function Comments(){
     const comments = ['Comment', 'Comment', 'Comment', 'Comment'];
 
     return (
-        <div className="p-2 mx-4 mb-4 flex flex-col section-pwhite border-x-2 border-b-2 rounded-b-md border-pdark">
+        <div className="py-2 mx-4 mb-2">
+            <div className="p-2 border-x-2 border-t-2 rounded-t-md border-pdark">Comments</div>
+            <div className="p-2 flex flex-col section-pwhite border-x-2 border-y-2 rounded-b-md border-pdark">
             <div className="grid grid-rows-4 m-8">
                 {comments.map((comment, index) =>
                     <div key={index}>{comment}</div>
                 )}
             </div>
         </div>
+        </div>
+        
     )
 }
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,7 +3,7 @@ import Comments from "./comments";
 export default function Post(){
 
     return (
-    <div className="flex items-center justify-center w-full h-96">
+    <div className="flex-column w-full h-96">
         <h3>Post Title</h3>       
         <p>post content</p>
         <Comments></Comments>
